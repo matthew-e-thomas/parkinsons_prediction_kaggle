31e42f8af532a780ec8f10ed6357bbbb7f11e757
matthew-e-thomas <matt.thomas@email.virginia.edu>
2023-02-22 08:23:07 -0500

    update prepare_data.py

--- scripts/xgboost/prepare_data.py
+++ scripts/xgboost/prepare_data.py
@@ -9,4 +9,9 @@ def prepare_training_data(protein_path: Path = PROTEIN_PATH,
                           peptide_path: Path = PEPTIDE_PATH,
                           clinical_path: Path = CLINICAL_PATH) -> pd.DataFrame:
 
-    df = pd.read_csv(file_path)
+    df_proteins = pd.read_csv(protein_path)
+    df_peptides = pd.read_csv(peptide_path)
+    df_clinical = pd.read_csv(clinical_path)
+
+    df_proteins_peptides = df_proteins.merge(df_peptides, how='inner', on = ['visit_id', 'visit_month', 'patient_id', 'UniProt'])
+    df_train = df_proteins_peptides.merge(df_clinical, how='left', on=['visit_id', 'visit_month', 'patient_id'])
